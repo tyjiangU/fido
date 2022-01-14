00dd9436f87b62a4527f807df8d70d1f0a1b1ca0
tyjiangU <77828381+tyjiangU@users.noreply.github.com>
2022-01-13 21:30:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -598,7 +598,7 @@ def main():
             result = dict((k + "_{}".format(global_step), v) for k, v in result.items())
             results.update(result)
 
-    # Predication
+    # Prediction
     if args.do_predict:
         checkpoints = [args.output_dir]
         logger.info("Evaluate the following checkpoints: %s", checkpoints)
